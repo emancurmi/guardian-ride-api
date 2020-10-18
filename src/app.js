c42a3d09df9a93c54ae8b2dfa60ee5b89bb9c230
Eman Curmi <emancurmi@gmail.com>
2020-10-17 23:39:05 -0400

    whitelist

--- src/app.js
+++ src/app.js
@@ -28,24 +28,18 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test'
 }))
 
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
-//app.use(cors({
-//    origin: function (origin, callback) {
-//        // allow requests with no origin 
-//        if (!origin) return callback(null, true);
-//        if (whitelist.indexOf(origin) === -1) {
-//            var message = `The CORS policy for this origin doesn't ` +
-//                `allow access from the particular origin.`;
-//            return callback(new Error(message), false);
-//        }
-//        return callback(null, true);
-//    }
-//}));
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        if (!origin) return callback(null, true);
+        if (whitelist.indexOf(origin) === -1) {
+            var message = `The CORS policy for this origin doesn't ` +
+                `allow access from the particular origin.`;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(helmet())
 //app.use(validateBearerToken)
