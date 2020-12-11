9069c1038851506c6d6d1789f0c1fdf8bec84884
Eman Curmi <emancurmi@gmail.com>
2020-12-10 21:26:12 -0500

    update database connection

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     PORT: process.env.PORT || 8137,
     NODE_ENV: process.env.NODE_ENV || 'production',
     API_TOKEN: process.env.API_TOKEN || '7b967bbe-afc2-11ea-b3de-0242ac130004',
-    //DB_URL: process.env.DB_URL || 'postgres://tebymszlqvkfmh:b906bfac8291d770e0e2b88a37f403bf10ccd20cb9ba8fb72b6728fddc699255@ec2-23-20-168-40.compute-1.amazonaws.com:5432/d92tfic84kr9fc',
-    //DATABASE_URL: process.env.DATABASE_URL || 'postgres://tebymszlqvkfmh:b906bfac8291d770e0e2b88a37f403bf10ccd20cb9ba8fb72b6728fddc699255@ec2-23-20-168-40.compute-1.amazonaws.com:5432/d92tfic84kr9fc',
+    DB_URL: process.env.DB_URL || 'postgres://mzncziuayodpkz:5f259572bb0e0fba83a2129763e9a9f1bc5e1b6d4f1bf4507d71184919f49f2f@ec2-100-25-231-126.compute-1.amazonaws.com:5432/d4i3f6ebq1pdjn',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://mzncziuayodpkz:5f259572bb0e0fba83a2129763e9a9f1bc5e1b6d4f1bf4507d71184919f49f2f@ec2-100-25-231-126.compute-1.amazonaws.com:5432/d4i3f6ebq1pdjn',
 }
